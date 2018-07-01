debb5454546efb3a24ea86ca5f9fbc0d58185a4e
Meowchelle <txf0990@gmail.com>
2018-07-01 18:28:35 +1000

    This is the first version of the postboard

---
+++ postboard.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import sqlite3
+
+class Database(object):
+    'this is the database.'
+    def __init__(self, filename):
+        self.conn = sqlite3.connect(filename)
+        self.c = self.conn.cursor()
+    def addEntry(self, entry):
+        self.c.execute("CREATE TABLE IF NOT EXISTS board(ID integer PRIMARY KEY UNIQUE, content text)")
+        self.c.execute("INSERT INTO board(content) Values(?)", (entry,))
+        self.conn.commit()
+    def showDatabase(self):
+        result = []
+        for row in self.c.execute('SELECT * FROM board'):
+            result.append(row)
+        return result
+
+#def addEntry(entry, database):
+#    conn = sqlite3.connect(database)
+#    c = conn.cursor()
+#    c.execute("CREATE TABLE IF NOT EXISTS board(ID integer PRIMARY KEY UNIQUE, content text)")
+#    c.execute("INSERT INTO board(content) Values(?)", (entry,))
+#    conn.commit()
+#
+#def showDatabase(database):
+#    result = []
+#    conn = sqlite3.connect(database)
+#    c = conn.cursor()
+#    for row in c.execute('SELECT*FROM board'):
+#        result.append(row)
+#    return result
---
+++ server.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from flask import Flask, render_template, request
+import sqlite3
+from postboard import Database
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'This is the home page.'
+
+@app.route('/hello')
+def welcome():
+    return 'Hello!'
+
+@app.route('/hello/<username>')
+def hello_sb(username):
+    return 'Hello, %s!' % username
+
+@app.route('/user')
+def user():
+    return 'This is a user page.'
+
+@app.route('/user/<username>')
+def user_sb(username):
+    return 'This is %s\'s user page.' % username
+
+@app.route('/postboard', methods=['GET', 'POST'])
+def visit_postboard():
+    database = Database('post.db')
+    if request.method == 'POST':
+        database.addEntry(request.form['content'])
+    posts = database.showDatabase() # posts is a list of tuple.
+    return render_template('postboard.html', posts=posts)
+
+if __name__ == '__main__':
+    app.run()
